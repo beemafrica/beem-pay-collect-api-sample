8bb3177b52d4d9c1b4a5f23df1bf0a39fdd7acfa
stephano <stephano@bongolive.co.tz>
2020-02-10 01:11:25 +0300

    initial commit v0.0.1

---
+++ callback/node-js/callback.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//  parameters to be passed 
+
+app.post("/", (req, res) => {
+let {
+       transaction_id,
+       remote_transaction_id,
+       successful,
+       amount_collected,
+       currency_from,
+       currency_to,
+       exchange_rate,
+       subscriber_msisdn,
+       reference_number,
+       paybill_number,
+       timestamp,
+       network_mcc,
+       network_mnc,
+       network_name
+}
+    } = req.body;
+
+     // sending response.
+    res.json({
+        transaction_id,
+        "successful": "true"
+    });
+
+    });
+
+app.listen(7000, () => {
+console.log("app running on port 7000");
+});
